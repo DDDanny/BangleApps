a8013dcc1bf15f2109f52dff0c46830aa6597629
Danny <31635744+DDDanny@users.noreply.github.com>
2022-01-11 08:16:20 +0100

    refactor and settings

--- apps/sleepphasealarm/app.js
+++ apps/sleepphasealarm/app.js
@@ -1,67 +1,77 @@
-<<<<<<< Updated upstream
-const alarms = require("Storage").readJSON("alarm.json",1)||[];
-const active = alarms.filter(a=>a.on);
-=======
 /*
-  App needs at least one active future alarm sein in alarm or qalarm app
+  App needs at least one active future alarms in alarm or qalarm app
   A valid alarm is consedered when newer than (default: 30 minutes) from now
   Sleepphase begins earliest 10 minutes after non Movement
+
+  state flow:
+    #1  undefined:wait-data ->
+    #2    (no move >= 10 minutes) -> 1:sleep
+    #3      (on move) -> -1:waked-up
+    #4        (on remove before 10 minutes) -> 0:awake
+    #5        or #2
+    #6    (on move) -> 0:awake
 */
-const SETTINGS = require("Storage").readJSON("sleepphasealarm.json",1)||{showTimeSec: false, preAlertMinutes: 30};
-const ACTIVE = ((require("Storage").readJSON("alarm.json",1)||[]).concat(require("Storage").readJSON("qalarm.json",1))||[]).filter(a=>a.on);
-const ALERT_BEFORE_MS = SETTINGS.preAlertMinutes * 60000; //30 minutes
->>>>>>> Stashed changes
+const DEFAULT_SETTINGS = {
+  showTimeSec:false, //true,'unlocked'
+  preAlertMinutes:30, 
+  sleepThreshSec=10, 
+  moveThres=(process.env.HWVERSION==2) ? 0.008 : 0.006, //Bangle 2 sensor seems more sensitive
+  disableBangleWakeOn=true //deactivate twist etc.
+};
+const SETTINGS         = require("Storage").readJSON("sleepphasealarm.json",1)||DEFAULT_SETTINGS;
+//const SETTINGS = DEFAULT_SETTINGS; //DEV-Option
+const ACTIVE_TIMERS    = ((require("Storage").readJSON("alarm.json",1)||[]).concat(require("Storage").readJSON("qalarm.json",1))||[]).filter(a=>a.on);
+const ALERT_BEFORE_MS  = SETTINGS.preAlertMinutes * 60000; //30 minutes
 
 // Sleep/Wake detection with Estimation of Stationary Sleep-segments (ESS):
 // Marko Borazio, Eugen Berlin, Nagihan Kücükyildiz, Philipp M. Scholl and Kristof Van Laerhoven, "Towards a Benchmark for Wearable Sleep Analysis with Inertial Wrist-worn Sensing Units", ICHI 2014, Verona, Italy, IEEE Press, 2014.
 // https://ubicomp.eti.uni-siegen.de/home/datasets/ichi14/index.html.en
 //
-// Function needs to be called for every measurement but returns a value at maximum once a second (see winwidth)
+// Function needs to be called for every measurement but returns a value at maximum once a second (see winWidth)
 // start of sleep marker is delayed by sleepthresh due to continous data reading
-const winwidth=13;
-const nomothresh=(process.env.HWVERSION==2) ? 0.008 : 0.006;
-const sleepthresh=600;
-var ess_values = [];
-var slsnds = 0;
+const winWidth=13;
+var essValues = [];
 
-var slpPairs = []; //collects
+var slpSec = 0;
 var slpStart = undefined;
+var slpPairs = []; //collects sleep periods = start/end 
 
-function calc_ess(val) {
-  ess_values.push(val);
+function calcESS(val) {
+  const moveThres = SETTINGS.accelThres;
+  const sleepThreshSec = SETTINGS.sleepThreshSec * 60;
+  essValues.push(val);
 
-  if (ess_values.length == winwidth) {
+  if (essValues.length >= winWidth) {
     // calculate standard deviation over ~1s 
-    const mean = ess_values.reduce((prev,cur) => cur+prev) / ess_values.length;
-    const stddev = Math.sqrt(ess_values.map(val => Math.pow(val-mean,2)).reduce((prev,cur) => prev+cur)/ess_values.length);
-    ess_values = [];
+    const mean = essValues.reduce((prev,cur) => cur+prev) / essValues.length;
+    const stddev = Math.sqrt(essValues.map(val => Math.pow(val-mean,2)).reduce((prev,cur) => prev+cur) / essValues.length);
+    essValues = [];
 
-    // check for non-movement according to the threshold
-    const nonmot = stddev < nomothresh;
+    const nonmot = stddev < moveThres; // check for non-movement according to the threshold
+    console.log(Bangle.getOptions().powerSave||undefined, stddev, moveThres, nonmot, slpSec, slpSec >= sleepThreshSec, deepslpsnds); //DEV-option
 
-    // amount of seconds within non-movement sections
-    console.log(stddev, nomothresh, nonmot, slsnds, slsnds >= sleepthresh, deepslpsnds);
-    if (nonmot) {
-      slsnds+=1;
-      if (slsnds >= sleepthresh) {
+    if (nonmot) { //stationary
+      slpSec += 1;
+      if (slpSec >= sleepThreshSec) {
         if (!slpStart) slpStart = new Date(); //remember sleep-start
-        return true; // sleep
+        return 1; // sleeping
       }
-    } else {
-      slsnds=0;
-      if (slpStart) {
+    } else { //moved
+      slpSec=0;
+      if (slpStart) { //waked-up
         slpPairs.push({"start": slpStart, "end": new Date()}) //add pair
-        slpStart = undefined;
+        slpStart = undefined; //reset start
+        return -1; //waked-up
       }
-
-      return false; // awake
+      return 0; //awake
     }
+    return undefined; //no-state yet
   }
 }
 
 // future alarm from (q)alarm app 
 var nextAlarm;
-ACTIVE.forEach(a => {
+ACTIVE_TIMERS.forEach(a => {
   const NOW = new Date();
   var dateAlarm;
   if (a.hasOwnProperty('hr')) { //alarm app a.hr
@@ -85,17 +95,21 @@ ACTIVE.forEach(a => {
 });
 
 const LINES = 4; //0 - 3
-var L_HEIGTH; //calculated after laod/draw widgets!
+var L_HEIGTH; //calculated after load/draw widgets!
 var CENTER_X;
-function initScreen() {
+function init() {
   L_HEIGTH = Bangle.appRect.h/LINES;
   CENTER_X = Bangle.appRect.w/2+Bangle.appRect.x;
+
+  if (disableBangleWakeOn) { //BTNs are still active
+    wakeOns = ['wakeOnFaceUp', 'wakeOnTouch', 'wakeOnTwist'];
+  }
 }
 
 function drawStringLine(s, lineNo, forceSize) {
   g.clearRect(Bangle.appRect.x, lineNo*L_HEIGTH+Bangle.appRect.y, Bangle.appRect.x2, lineNo*L_HEIGTH+L_HEIGTH+Bangle.appRect.y);
 
-  /*switch (lineNo) {
+  /* switch (lineNo) { //DEV-Option
     case 1:
       g.setColor('#FF0000'); break;
     case 2:
@@ -105,7 +119,9 @@ function drawStringLine(s, lineNo, forceSize) {
     default:
       g.setColor('#000000'); break;
   }
-  g.fillRect(Bangle.appRect.x, CENTER_LINE_Y, Bangle.appRect.x2, CENTER_LINE_Y+L_HEIGTH);*/
+  g.fillRect(Bangle.appRect.x, CENTER_LINE_Y, Bangle.appRect.x2, CENTER_LINE_Y+L_HEIGTH);
+  */
+  
   g.reset();
   g.setFont("Vector", forceSize || L_HEIGTH*0.8);
   g.setFontAlign(0, 0); // center/center
@@ -118,9 +134,9 @@ function drawApp() {
   var aHr = nextAlarm.getHours();
   var aMin = nextAlarm.getMinutes();
   var aDayDelta = (nextAlarm.getDay()-NOW.getDay()+7)%7;
-  drawStringLine("Sleep Phase Alarm", 0, 18);
+  drawStringLine("Sleep Phase Alarm", 0, 18); //TODO: locale
   drawStringLine((aHr < 10 ? "0" : "") + aHr + ":" + (aMin < 10 ? "0" : "") + aMin + (aDayDelta > 0 ? " +" + aDayDelta  + "d" : "" ), 1);
-  drawStringLine("wait state", 2);
+  //drawStringLine("wait-state", 2);
   function drawTime() {
     if (Bangle.isLCDOn()) {
       const NOW = new Date();
@@ -129,7 +145,7 @@ function drawApp() {
       var tSec = NOW.getSeconds();
       drawStringLine((tHr < 10 ? "0" : "") + tHr + ":" + (tMin < 10 ? "0" : "") + tMin + (SETTINGS.showTimeSec ? ":" + (tSec < 10 ? "0" : "") + tSec : ""), 3);
     }
-    if (SETTINGS.showTimeSec)
+    if (SETTINGS.showTimeSec === true || SETTINGS.showTimeSec === 'unlocked' && Bangle.isLocked() === false)
       setTimeout(drawTime, 1000 - NOW.getMilliseconds()); // schedule to next second
     else
       setTimeout(drawTime, 60000 - (NOW.getSeconds() * 1000) - NOW.getMilliseconds()); // schedule to next minute
@@ -155,29 +171,33 @@ function buzz() {
 // run
 var minAlarm = new Date();
 var measure = true;
-var totalSleep;
+var totalSleepSec;
 if (nextAlarm !== undefined) {
   Bangle.loadWidgets();
   Bangle.drawWidgets();
-  initScreen();
+  initScreen(); //detect upper and or lower widgets-area
   minAlarm.setTime(nextAlarm.getTime() - (ALERT_BEFORE_MS)); // minimum alert 30 minutes early
   setInterval(function() {
     const now = new Date();
-    const acc = Bangle.getAccel().mag;
-    const swest = calc_ess(acc);
+    const swest = calcESS(Bangle.getAccel().mag);
 
-    if (swest !== undefined) {
-      if (Bangle.isLCDOn()) {
-        drawStringLine(swest ? "sleep" : "awake", 2);
+    if (Bangle.isLCDOn()) {
+      let stateTxt;
+      switch (swest) { //TODO: locale
+        case undefined: stateTxt="wait-data"; break;
+        case -1: stateTxt="waked-up"; break;
+        case  1: stateTxt="sleeping"; break;
+        default: stateTxt="awake"; break;
       }
+      drawStringLine(stateTxt, 2);
     }
 
-    if (now >= nextAlarm || (measure && now >= minAlarm && swest === false)) {
+    if (now >= nextAlarm || (measure && now >= minAlarm && swest <= 0)) {
       measure = false;
-      if (totalSleep === undefined) {
-        if (slpStart !== undefined) slpPairs.push({"start": slpStart, "end": new Date()}); //collect last open interval
-        totalSleep = slpPairs.reduce((sum, pair) => sum += pair.end - pair.start)
-      // The alarm widget should handle this one
+      if (totalSleepSec === undefined) {
+        if (slpStart !== undefined) slpPairs.push({"start":slpStart, "end":new Date()}); //collect last open interval
+        totalSleepSec = slpPairs.reduce((sum, period) => sum += period.end - period.start)
+        // The alarm widget should handle this one
         E.showMessage(deepslpsecs);
       }
       //setTimeout(load, 1000);
@@ -188,7 +208,7 @@ if (nextAlarm !== undefined) {
   }, 80); // 12.5Hz
   drawApp();
 } else {
-  E.showMessage('No active (q)alarm!');
+  E.showMessage('No active (q)alarm!'); //TODO: locale
   setTimeout(load, 1000);
 }
 // BTN2 to menu, BTN3 to main
